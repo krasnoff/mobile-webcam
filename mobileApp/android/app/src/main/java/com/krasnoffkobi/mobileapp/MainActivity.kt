8ef82f3c716f96c20e26f735f4beea9d303de5d3
Kobi Krasnoff <krasnoff.kobi@gmail.com>
2024-10-06 21:06:18 +0300

    change package directory

--- mobileApp/android/app/src/main/java/com/krasnoffkobi/mobileapp/MainActivity.kt
+++ mobileApp/android/app/src/main/java/com/krasnoffkobi/mobileapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.mobileapp
+package com.krasnoffkobi.mobileapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- mobileApp/android/app/src/main/java/com/krasnoffkobi/mobileapp/MainApplication.kt
+++ mobileApp/android/app/src/main/java/com/krasnoffkobi/mobileapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.mobileapp
+package com.krasnoffkobi.mobileapp
 
 import android.app.Application
 import com.facebook.react.PackageList
